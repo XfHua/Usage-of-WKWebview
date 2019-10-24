385c5714b0deb86131580f6afc91f9cff2e26d0d
XfHua <jasonHuaxf@163.com>
2019-10-24 13:34:22 +0800

    Add files via upload

---
+++ App17/App17/App17.iOS/MyWebViewRenderer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using App17;
+using App17.iOS;
+using Foundation;
+using UIKit;
+using WebKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(MyWebView), typeof(MyWebViewRenderer))]
+namespace App17.iOS
+    {
+        public class MyWebViewRenderer : ViewRenderer<MyWebView, WKWebView>
+        {
+            WKWebView _wkWebView;
+            protected override void OnElementChanged(ElementChangedEventArgs<MyWebView> e)
+            {
+                base.OnElementChanged(e);
+
+                if (Control == null)
+                {
+                    var config = new WKWebViewConfiguration();
+                    _wkWebView = new WKWebView(Frame, config);
+                    SetNativeControl(_wkWebView);
+                }
+                if (e.NewElement != null)
+                {
+                    Control.LoadRequest(new NSUrlRequest(new NSUrl(Element.Url)));
+                }
+            }
+        }
+    }
---
+++ App17/App17/App17/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App17
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class MyWebView : WebView
+    {
+        public static readonly BindableProperty UrlProperty = BindableProperty.Create(
+            propertyName: "Url",
+            returnType: typeof(string),
+            declaringType: typeof(MyWebView),
+            defaultValue: default(string));
+
+        public string Url
+        {
+            get { return (string)GetValue(UrlProperty); }
+            set { SetValue(UrlProperty, value); }
+        }
+    }
+}
